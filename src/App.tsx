4729508d00f179823b0884b2795de238363ba814
G <g@pop-os.lan>
2024-12-29 22:36:27 +0100

    swapy corrections on items

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
+import { useEffect, useRef } from "react";
 import { CardWithForm } from "./components/CardWithForm";
 import { Header } from "./components/Header";
+import { createSwapy, Swapy } from "swapy";
 
 function App() {
+  const swapyRef = useRef<Swapy | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      console.log("Container HTML:", containerRef.current.innerHTML);
+
+      swapyRef.current = createSwapy(containerRef.current, {
+      });
+
+      swapyRef.current.onSwapStart((event) => {
+        console.log("Swap Start:", event);
+      });
+      swapyRef.current.onSwap((event) => {
+        console.log("Swapping:", event);
+      });
+      swapyRef.current.onSwapEnd((event) => {
+        console.log("Swap End:", event);
+      });
+    }
+
+    return () => {
+      swapyRef.current?.destroy();
+    };
+  }, []);
+
   return (
-    <>
-      <div className="bg-slate-900 text-white min-h-screen">
-        <Header />
-        <section className="mt-4 container">
+    <div className="bg-slate-900 text-white min-h-screen">
+      <Header />
+      <section className="mt-4">
+        <div className="container" ref={containerRef}>
           <div className="flex justify-center gap-4 mt-4" data-swapy-slot="1">
-            <CardWithForm data-swapy-item="a" />
+            <div data-swapy-item="1">
+              <CardWithForm />
+            </div>
           </div>
           <div className="flex justify-center gap-4 mt-4" data-swapy-slot="2">
-            <CardWithForm data-swapy-item="b" />
+            <div data-swapy-item="2">
+              <CardWithForm />
+            </div>
           </div>
           <div className="flex justify-center gap-4 mt-4" data-swapy-slot="3">
-            <CardWithForm data-swapy-item="c" />
+            <div data-swapy-item="3">
+              <CardWithForm />
+            </div>
           </div>
-        </section>
-      </div>
-    </>
+        </div>
+      </section>
+    </div>
   );
 }
 
